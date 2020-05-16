ed0be3b4e2bb7eadbbf764c5ac9fa4d5f22444a2
Gabriel <gabrielcostamoura1@gmail.com>
2020-05-15 23:28:55 -0300

    :art: upgraded header of script and added cont to each algoritmo

--- EPs/EP1/EP1.py
+++ EPs/EP1/EP1.py
@@ -3,22 +3,45 @@ import random
 import time
 
 # Gera lista de acordo com o tamanho passado
- 
+
+
 def geraLista(n):
     lista = list(range(n))
     random.shuffle(lista)
     return lista
 
-# Algoritmo de Seleção 
+# Algoritmo de Seleção
+
+
 def seleção(lista):
-  vet = []
-  while lista:
-    menor = min(lista)
-    vet.append(menor)
-    lista.remove(menor)
-  return vet
+    vet = []
+    global contSelec
+    contSelec = 0
+    while lista:
+        contSelec += 1
+        menor = min(lista)
+        vet.append(menor)
+        lista.remove(menor)
+    return vet
+
+
+# Algoritmo de Inserção
+def inserção(v):
+    global contInsert
+    contInsert = 0
+    for j in range(1, len(v)):
+        x = v[j]
+        i = j - 1
+        while i >= 0 and v[i] > x:
+            v[i + 1] = v[i]
+            i = i - 1
+            contInsert = contInsert + 1
+        v[i + 1] = x
+        return v
+
 
 # Algoritmo MergeSort
+
 
 def merge(esquerda, direita):
     vet = []
@@ -34,27 +57,37 @@ def merge(esquerda, direita):
     vet += direita[j:]
     return vet
 
+
 def mergesort(v):
+    global contMerge
+    contMerge = 0
     if len(v) <= 1:
         return v
     else:
         m = len(v) // 2
         e = mergesort(v[:m])
         d = mergesort(v[m:])
+        contMerge += 1
         return merge(e, d)
 
-#Algoritmo quicksort
-def quick(lista):
-   if len(lista) <= 1:
-       return lista
+# Algoritmo quicksort
 
-   inicio = lista[0]
-   iguais  = [x for x in lista if x == inicio]
-   menores = [x for x in lista if x <  inicio]
-   maiores = [x for x in lista if x >  inicio]
-   return quick(menores) + iguais + quick(maiores)
+
+def quick(lista):
+    global contQuick
+    contQuick = 0
+    if len(lista) <= 1:
+        return lista
+    inicio = lista[0]
+    iguais = [x for x in lista if x == inicio]
+    menores = [x for x in lista if x < inicio]
+    maiores = [x for x in lista if x > inicio]
+    contQuick += 1
+    return quick(menores) + iguais + quick(maiores)
 
 # Cronometra tempo de execução da função nativa .sort()
+
+
 def cronometraNative(lista):
     inicio = time.time()
     lista.sort()
@@ -63,6 +96,8 @@ def cronometraNative(lista):
     tempoNative = fim - inicio
 
 # Cronometra o tempo de execução da função de seleção
+
+
 def cronometraSeleção(lista):
     lista = lista.copy()
     inicio = time.time()
@@ -71,7 +106,18 @@ def cronometraSeleção(lista):
     global tempoSeleção
     tempoSeleção = fim-inicio
 
+
+def cronometraInserção(lista):
+    lista = lista.copy()
+    inicio = time.time()
+    inserção(lista)
+    fim = time.time()
+    global tempoInserção
+    tempoInserção = fim - inicio
+
 # Cronometra o tempos de execução da função Merge
+
+
 def cronometraMerge(lista):
     lista = lista.copy()
     inicio = time.time()
@@ -81,6 +127,8 @@ def cronometraMerge(lista):
     tempoMerge = fim-inicio
 
 # Cronometra o tempo de execução da função Quick
+
+
 def cronometraQuick(list):
     lista = list.copy()
     inicio = time.time()
@@ -89,25 +137,32 @@ def cronometraQuick(list):
     global tempoQuick
     tempoQuick = fim-inicio
 
-print("---------------------------------------------------------------------------------------")
-print("                                         Tempo                                         ")
-print("---------------------------------------------------------------------------------------")
-print("   "
-      "          |         Mergesort   Quicksort   Selection   Native         |            ")
 
-size = 2000
+size = 5000
 i = 0
-for i in range(11):
+cronometro = 0
+print("----------------------------------------------------------------------------------------------------------------------|")
+print(
+    "|                                            [EP1] - Vale a pena ordenar?                                               |")
+print('| Algoritmo escolhido: Todos                                             Duração dos testes 30.00                     |')
+print('| Alunos: Gabriel Costa, Raphael Ribeira de Paula e William Barreto                                                   |')
+print("|---------------------------------------------------------------------------------------------------------------------|")
+
+print("|---------------------------Tempo de Ordenação----------------------|--------------Numero de Buscas-------------------|")
+print('|-------------------------------------------------------------------|-------------------------------------------------|')
+print("|   n     |  Inserção   Mergesort   Quicksort   Selection   Native  |   Inserção   Mergesort   Quicksort   Selection |")
+inicio = time.time()
+while cronometro < 30:
     lista = geraLista(size)
     cronometraSeleção(lista)
     cronometraMerge(lista)
     cronometraQuick(lista)
     cronometraNative(lista)
-        
-    i =(tempoMerge+tempoQuick+tempoSeleção)
-    if(size < 10000):
-        print("   %.0f" % size, "     |","         %.2f" % tempoMerge,"        %.2f" % tempoQuick,"        %.2f" % tempoSeleção,"     %.3f"%tempoNative,"        |")
-    else:
-        print("   %.0f" % size, "    |", "         %.2f" % tempoMerge, "        %.2f" % tempoQuick, "        %.2f" % tempoSeleção,"     %.3f"%tempoNative,"        |" )
-    size += 2000
-    
+    cronometraInserção(lista)
+
+    i = (tempoMerge+tempoQuick+tempoSeleção+tempoInserção)
+    print("|   %.0f" % size, " |", "   %.2f" % tempoInserção, "       %.2f" % tempoMerge,
+          "        %.2f" % tempoQuick, "       %.2f" % tempoSeleção, "   %.3f" % tempoNative, " |      %d" % contInsert,  "       %d" % contMerge,  "          %d" % contQuick,  "        %d       |" % contSelec)
+
+    size += 5000
+    cronometro = time.time() - inicio
